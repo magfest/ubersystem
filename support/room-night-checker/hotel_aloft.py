38502b1bdf49ff776f0ebfcf53f7eecaf485166b
binary1230 <none@none>
2013-12-02 10:09:05 -0500

    removed hotel room night checker from uber (this has now been extracted into a new mercurial repo).  this was never actually part of uber, just was convenient to put in here.  now it's all grown up and leaving the house, soon it'll have kids and I'll be a grandparent, just like I always wanted.  these dang hotels grow up so fast. -Dom

--- support/room-night-checker/hotel_aloft.py
+++
@@ -1,6 +0,0 @@
-from hotel_starwood_base import *
-
-class ALoftHotelRoomChecker(StarwoodHotelRoomChecker):
-    def __init__(self):
-        self.hotel_url = 'https://www.starwoodmeeting.com/StarGroupsWeb/res?id=1310291105&key=8986D'
-        self.hotel_name = 'ALoft'
--- support/room-night-checker/hotel_westin.py
+++
@@ -1,6 +0,0 @@
-from hotel_starwood_base import *
-
-class WestinHotelRoomChecker(StarwoodHotelRoomChecker):
-    def __init__(self):
-        self.hotel_url = 'https://www.starwoodmeeting.com/StarGroupsWeb/res?id=1310219832&key=DE27A'
-        self.hotel_name = 'Westin'
