9ff7b1b45e121b7ce2c0faa4690ebe57720fab8f
eli <none@none>
2014-01-27 12:46:48 -0500

    #180: added redirector

---
+++ uber/http_to_https.py
@@ -0,0 +1,12 @@
+from uber.common import *
+
+class Redirector:
+    @cherrypy.expose
+    def default(self, *args, **kwargs):
+        raise HTTPRedirect('{}{req.path_info}?{req.query_string}'.format(URL_BASE, req=cherrypy.request))
+
+if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_port': 80})
+    cherrypy.tree.mount(Redirector(), '/')
+    cherrypy.engine.start()
+    cherrypy.engine.block()
