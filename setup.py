14f5af3d93aeb8282e6b25d871eed796605b1db8
Eli Courtwright <eli@courtwright.org>
2014-02-01 00:03:23 -0500

    removed old files and refactored setup.py to use requirements.txt in preparation for heroku

--- setup.py
+++ setup.py
@@ -8,14 +8,6 @@ if __name__ == '__main__':
         author='Eli Courtwright',
         author_email='eli@courtwright.org',
         description='The MAGFest Ubersystem',
-        url='https://bitbucket.org/EliAndrewC/magfest',
-        install_requires = [
-            "Django==1.6.1",
-            "psycopg2==2.5.2",
-            "py3k-bcrypt==0.3",
-            "logging_unterpolation==0.2.0",
-            "stripe==1.11.0",
-            "CherryPy==3.2.4",
-            "configobj-py3==4.7.2",
-        ]
+        url='https://github.com/EliAndrewC/magfest',
+        install_requires=open('requirements.txt').readlines()
     )
