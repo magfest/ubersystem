f237be35d1868674dcdc66ec6aa511fb92e3cbed
Dom <binary1230@gmail.com>
2014-05-26 22:20:55 -0400

    created a new tag to elegantly display "organization" and "event name" together, and only display one if they're both equal
    fixes #226

--- uber/custom_tags.py
+++ uber/custom_tags.py
@@ -407,6 +407,13 @@ class BoldIfNode(template.Node):
             return output
 
 
+@tag
+class organization_and_event_name(template.Node):
+    def render(self, context):
+        if EVENT_NAME.lower() != ORGANIZATION_NAME.lower():
+            return EVENT_NAME + ' and ' + ORGANIZATION_NAME
+        else:
+            return EVENT_NAME
 
 @tag
 class single_day_prices(template.Node):
