0e5cdba577fa1bb6dbda5fc9145b171c0b4504ef
Dom <binary1230@gmail.com>
2014-05-29 02:14:50 -0400

    redo theme support in a non-dumb way
    Man. what was I thinking? Discovered django had a really easy way to do this with TEMPLATE_DIRS, so I did that.
    
    Now we can override ANY template, not just the ones in themed/ and/or static_views/ dir.
    
    example:  templates_themed/preregistration/disclaimers.html overrides th same file in templates/preregistration
    
    so:
    - renamed themed/ back to static_view/
    - created a new dir named templates_themed/
    - setup configspec.ini to setup TEMPLATES_DIR

--- uber/server.py
+++ uber/server.py
@@ -28,41 +28,23 @@ class UberShutDown:
 
 mimetypes.init()
 
-class StaticThemedViews:
+class StaticViews:
     def path_args_to_string(self, path_args):
         return '/'.join(path_args)
 
-    def build_theme_path(self, theme_name, path_args):
-        return theme_name + '/' + self.path_args_to_string(path_args)
-
-    def get_possible_theme_templates(self, path_args):
-        themed_template_name = self.build_theme_path(CURRENT_THEME, path_args)
-        unthemed_template_name = self.build_theme_path('common', path_args)
-
-        return [themed_template_name, unthemed_template_name]
-
-    def get_possible_templates(self, path_args):
-        path_names = []
-
-        # if they ask for                   my_file.html
-        # then:
-        # first we try                      {{CURRENT_THEME}}/my_file.html
-        # if it doesn't exist, we try       common/my_file.html
-        path_names = self.get_possible_theme_templates(path_args)
-
-        return ['themed/' + p for p in path_names]
+    def get_full_path_from_path_args(self, path_args):
+        return 'static_views/' + self.path_args_to_string(path_args)
 
     def get_filename_from_path_args(self, path_args):
         return path_args[-1]
 
     @cherrypy.expose
-    # TODO: nicer error handling -Dom
     def default(self, *args, **kwargs):
         path_args = args
         content_filename = self.get_filename_from_path_args(path_args)
 
-        possible_filenames_with_path = self.get_possible_templates(path_args)
-        content = render_from_list(possible_filenames_with_path)
+        template_name = self.get_full_path_from_path_args(path_args)
+        content = render(template_name)
 
         guessed_content_type = mimetypes.guess_type(content_filename)[0]
         return cherrypy.lib.static.serve_fileobj(content, name=content_filename, content_type=guessed_content_type)
@@ -76,7 +58,7 @@ class Uber:
         cherrypy.response.headers['Content-Type'] = 'text/javascript'
         return render('common.js')
 
-    themed = StaticThemedViews()
+    static_views = StaticViews()
 
 _sections = [path.split('/')[-1][:-3] for path in glob(os.path.join(MODULE_ROOT, 'site_sections', '*.py'))
                                       if not path.endswith('__init__.py')]
