122c9366a9c705c836a3ce4cbf6e802480e9ea40
Dominic Cerquetti <binary1230@gmail.com>
2014-08-13 09:37:56 -0400

    quick fix: get summary page working again
    remove old magstock shirt crud that doesn't work for magfest 8.5

--- uber/site_sections/summary.py
+++ uber/site_sections/summary.py
@@ -41,7 +41,7 @@ class Root:
             'free_group':    attendees.filter(paid=PAID_BY_GROUP, group__amount_paid=0).count(),
             'shirt_sales':   [(i, Attendee.objects.filter(registered__lte=datetime.now() - timedelta(days = i * 7)).exclude(shirt=NO_SHIRT).count()) for i in range(50)],
             'ribbons':       [(desc, Attendee.objects.filter(ribbon=val).count()) for val,desc in RIBBON_OPTS if val != NO_RIBBON],
-            'magstock_shirt_sales': get_magstock_shirt_summary(),
+            # 'magstock_shirt_sales': get_magstock_shirt_summary(),
         }
 
         return data
