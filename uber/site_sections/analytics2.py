700c4b34db03632daeafc946cd863f770ca31fe4
Dom <dom@magfest.org>
2014-08-18 13:04:43 -0400

    WIP start for new analytics code, Josh will be working on this

---
+++ uber/site_sections/analytics2.py
@@ -0,0 +1,21 @@
+from uber.common import *
+
+class GraphData:
+    def __init__(self):
+        self.event_name = "magfest 13"
+        self.registrations = [{'dec 18th': 37}, {'dec 20th': 40}]
+        self.event_end_date = "01/23/14"
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+@all_renderable(PEOPLE, STATS)
+class Root:
+    def index(self, session):
+        # attendees, groups = session.everyone()
+
+        graph_data = GraphData()
+        graph_data.junk = "sup"
+
+        return {'graph_data': graph_data.to_JSON()}
+
