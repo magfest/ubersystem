92308497d0038defa7c4d9604f46cf89de134174
Victoria Earl <kitsuta@gmail.com>
2014-09-29 15:12:44 -0400

    Front-end changes for birthdate
    Adds the front-end input fields for birthdate, including correct
    formatting.
    
    Also adds the jQueryUI datepicker class.

--- uber/templates/common.js
+++ uber/templates/common.js
@@ -97,3 +97,12 @@ function showTop(message) {
 function hideTop() {
     $("#top").hide("fast");
 }
+
+$(function() {
+    $(".datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "-100:+0",
+        defaultDate: "-20y"
+    });
+});
