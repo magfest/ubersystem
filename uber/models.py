932003b5cb79053002a2b25bc1c8f7e7760bfd28
Eli Courtwright <eli@courtwright.org>
2014-10-01 22:43:25 -0400

    fixed colon issue

--- uber/models.py
+++ uber/models.py
@@ -506,8 +506,8 @@ class Attendee(MagModel, TakesPaymentMixin):
 
         if self.badge_type == STAFF_BADGE and self.ribbon == VOLUNTEER_RIBBON:
             self.ribbon = NO_RIBBON
-            
-        if self.badge_type == STAFF_BADGE and not self.staffing
+
+        if self.badge_type == STAFF_BADGE:
             self.staffing = True
 
     def unset_volunteering(self):
