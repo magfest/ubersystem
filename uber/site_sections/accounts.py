497a4a42c988c4f8ce0f3d516cccb71647b44f5d
Dom <dom@magfest.org>
2014-10-03 11:52:20 -0400

    newsletter export fixes - move code from summary.py to accounts.py for security reasons (less people can access it) - filter_by() more concise

--- uber/site_sections/accounts.py
+++ uber/site_sections/accounts.py
@@ -122,6 +122,13 @@ class Root:
 
         return {'message': message}
 
+    # print out a CSV list of attendees that signed up for the newsletter for import into our bulk mailer
+    @csv_file
+    def can_spam(self, out, session):
+        out.writerow(["fullname", "email", "zipcode"])
+        for a in session.query(Attendee).filter_by(can_spam=True).order_by('email').all():
+            out.writerow([a.full_name, a.email, a.zip_code])
+
     @unrestricted
     def sitemap(self):
         from uber import site_sections
