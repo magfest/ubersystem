231f68f1e7a02815bc494af067970f98c88de2b8
Dom <dom@magfest.org>
2014-11-20 00:09:23 -0500

    copy debugger and debug server stuff from uber plugin into sideboard

--- uber/run_debug_server.py
+++
@@ -1,9 +0,0 @@
-from uber.common import *
-from debugger import debugger_helpers_all_init
-
-if __name__ == '__main__':
-
-    debugger_helpers_all_init()
-
-    cherrypy.engine.start()
-    cherrypy.engine.block()
