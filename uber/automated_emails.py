42d233be41c9de9fbb709e28f118d02eb1dcf8e8
Victoria Earl <kitsuta@gmail.com>
2014-12-05 10:43:20 -0500

    Reduce number of emails for staff
    Staff currently get four simultaneous emails when added the AnthroCon
    way. This prevents the extraneous emails from getting sent.

--- uber/automated_emails.py
+++ uber/automated_emails.py
@@ -106,10 +106,10 @@ AutomatedEmail(Attendee, '{EVENT_NAME} payment received', 'reg_workflow/attendee
          lambda a: a.paid == HAS_PAID)
 
 AutomatedEmail(Group, '{EVENT_NAME} group payment received', 'reg_workflow/group_confirmation.html',
-         lambda g: g.amount_paid == g.cost)
+         lambda g: g.amount_paid == g.cost and g.cost != 0)
 
 AutomatedEmail(Attendee, '{EVENT_NAME} group registration confirmed', 'reg_workflow/attendee_confirmation.html',
-         lambda a: a.group and a != a.group.leader)
+         lambda a: a.group and a != a.group.leader and not a.placeholder)
 
 AutomatedEmail(Attendee, '{EVENT_NAME} extra payment received', 'reg_workflow/group_donation.txt',
          lambda a: a.paid == PAID_BY_GROUP and a.amount_extra and a.amount_paid == a.amount_extra)
@@ -180,7 +180,7 @@ AutomatedEmail(Attendee, '{EVENT_NAME} Badge Confirmation', 'placeholders/regula
                                        and a.ribbon not in [DEALER_RIBBON, PANELIST_RIBBON, VOLUNTEER_RIBBON])
 
 AutomatedEmail(Attendee, '{EVENT_NAME} Badge Confirmation Reminder', 'placeholders/reminder.txt',
-               lambda a: days_after(7, a.registered) and a.placeholder and a.first_name and a.last_name and not a.is_dealer)
+               lambda a: days_after(21, a.registered) and a.placeholder and a.first_name and a.last_name and not a.is_dealer)
 
 AutomatedEmail(Attendee, 'Last Chance to Accept Your {EVENT_NAME} Badge', 'placeholders/reminder.txt',
                lambda a: days_before(7, PLACEHOLDER_DEADLINE) and a.placeholder and a.first_name and a.last_name
