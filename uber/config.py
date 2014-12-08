aa179feb65562e72cbb24c6de94c756897d5fb79
Victoria Earl <kitsuta@gmail.com>
2014-12-08 13:51:05 -0500

    Prevent injection for badges
    Catches badge injection serverside so enterprising dealers can't
    artificially increase their dealer assistant count.

--- uber/config.py
+++ uber/config.py
@@ -242,6 +242,8 @@ TABLE_OPTS = [
 
 TABLE_PRICES = {0: 0, 0.5: 40, 1: 100, 2: 350, 3: 600, 4: 999}
 
+MAX_DEALER_BADGES = {0: 1, 0.5: 2, 1: 2, 2: 3, 3: 4, 4: 5}
+
 NIGHT_DISPLAY_ORDER = [globals()[night.upper()] for night in NIGHT_DISPLAY_ORDER]
 NIGHT_NAMES = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
