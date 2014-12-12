56b2f0030758e3282e3f0037bd197fffcacdd4bb
Dom <dom@magfest.org>
2014-12-11 20:41:59 -0500

    don't export placeholder badges they are typically people who are coming back next year but can't staff this year

--- uber/site_sections/accounts.py
+++ uber/site_sections/accounts.py
@@ -127,7 +127,7 @@ class Root:
     @csv_file
     def can_spam(self, out, session):
         out.writerow(["fullname", "email", "zipcode"])
-        for a in session.query(Attendee).filter_by(can_spam=True).order_by('email').all():
+        for a in session.query(Attendee).filter_by(can_spam=True, placeholder=False).order_by('email').all():
             out.writerow([a.full_name, a.email, a.zip_code])
 
     @unrestricted
