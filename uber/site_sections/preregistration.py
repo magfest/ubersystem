33e77e5b3b65003530d5494257e5f8d589f499b7
Victoria Earl <kitsuta@gmail.com>
2014-12-13 14:04:53 -0500

    Typo fix
    Fixes a typo in one of the validation messages, and centers the "dealer
    application" heading.

--- uber/site_sections/preregistration.py
+++ uber/site_sections/preregistration.py
@@ -18,7 +18,7 @@ def check_dealer(group):
     if not group.address and COLLECT_INTERESTS:
         return 'Dealers are required to provide an address for tax purposes'
     elif not group.wares:
-        return 'You must provide a detail explanation of what you sell for us to evaluate your submission'
+        return 'You must provide a detailed explanation of what you sell for us to evaluate your submission'
     elif not group.website:
         if COLLECT_INTERESTS:
             return "Please enter your business' website address"
