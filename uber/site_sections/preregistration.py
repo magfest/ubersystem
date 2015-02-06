1c65fc6846f7bb82361dff418a80063571d88d24
Dominic Cerquetti <binary1230@gmail.com>
2015-02-06 13:51:29 -0800

    disable magstock shirt setting (rockage will use everything else).
    my god. the haxx. the haxx.

--- uber/site_sections/preregistration.py
+++ uber/site_sections/preregistration.py
@@ -103,10 +103,10 @@ class Root:
 
     @check_if_can_reg
     def form(self, session, message='', edit_id=None, **params):
-        if MODE == 'magstock':
-            if params.get('buy_shirt') != 'on':
-                params['shirt'] = NO_SHIRT
-                params['shirt_color'] = NO_SHIRT
+        #if MODE == 'magstock':
+        #    if params.get('buy_shirt') != 'on':
+        #        params['shirt'] = NO_SHIRT
+        #        params['shirt_color'] = NO_SHIRT
 
         if 'badge_type' not in params and edit_id is None:
             raise HTTPRedirect('badge_choice?message={}', 'You must select a badge type')
