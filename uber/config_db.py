c49f8e3266e14ebd3a76701e9250579d76d1af25
Victoria Earl <kitsuta@gmail.com>
2015-03-16 18:39:20 -0400

    Add an upgrade command for ac-merge
    Still needs some testing, but this should allow one to upgrade from
    ac-staging to ac-merge

--- uber/config_db.py
+++ uber/config_db.py
@@ -38,3 +38,15 @@ def add_age_groups():
             can_register   = True
         )
         session.add(age_group)
+
+@entry_point
+def upgrade_db():
+    connection = Session.engine.connect()
+    trans = connection.begin()
+    try:
+        sql_update = '''ALTER TABLE Job ADD COLUMN type INTEGER NOT NULL DEFAULT 252034462; ALTER TABLE Job ALTER COLUMN weight SET DEFAULT 1; ALTER TABLE food_restrictions ADD COLUMN sandwich_pref INTEGER NOT NULL DEFAULT 127073423; ALTER TABLE food_restrictions ADD COLUMN no_cheese BOOLEAN NOT NULL DEFAULT FALSE; DROP TABLE room CASCADE; DROP TABLE checkout;'''
+        result = connection.execute(sql_update)
+        trans.commit()
+    except:
+        trans.rollback()
+        raise
