102b434d449291bf0de9b63e585d6cb5eb8cf8c8
Ian Webb <i_webb@acs.org>
2015-03-23 15:15:07 -0400

    remove age groups

--- uber/config_db.py
+++ uber/config_db.py
@@ -4,37 +4,10 @@ from uber.common import *
 def add_age_groups():
     with Session() as session:
         age_group = AgeGroup(
-            desc           = 'age unknown',
+            desc           = 'n/a',
             min_age        = 0,
-            max_age        = 0,
+            max_age        = 999,
             discount       = 0,
-            can_register   = False
-        )
-        session.add(age_group)
-    
-        age_group = AgeGroup(
-            desc           = 'under 13',
-            min_age        = 0,
-            max_age        = 12,
-            discount       = 0,
-            can_register   = False
-        )
-        session.add(age_group)
-        
-        age_group = AgeGroup(
-            desc           = '13 to 18',
-            min_age        = 13,
-            max_age        = 17,
-            discount       = 0,
-            can_register   = True
-        )
-        session.add(age_group)
-        
-        age_group = AgeGroup(
-            desc           = '18 or over',
-            min_age        = 18,
-            max_age        = 150,
-            discount       = 10,
             can_register   = True
         )
         session.add(age_group)
