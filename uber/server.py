94fe1053820e51e4647b2e6e340583bd321fb92d
Dom <dom@magfest.org>
2015-06-07 12:54:41 -0400

    remove older Redirector() stuff
    need Eli's review of this one.
    
    1) http_to_https.py looks like an older way to start the server such that it forces everything to redirect from HTTP to HTTPS by relying on the (now-removed) behavior of HTTPRedirect to do this.  I'm -pretty sure- this file is old and can just be killed, so I deleted it.
    
    2) it appears way long ago that we used to do cherrypy.tree.mount("/", Redirector()) as a way of providing a default page. this looks to be pre-sideboard era, and we shouldn't do it anymore because sideboard mounts stuff in "/" so, I think we should just delete the Redirector() stuff.

--- uber/server.py
+++ uber/server.py
@@ -62,15 +62,6 @@ class Root:
 
 mount_site_sections(c.MODULE_ROOT)
 
-
-class Redirector:
-    @cherrypy.expose
-    def index(self):
-        if c.AT_THE_CON:
-            raise HTTPRedirect(c.PATH + '/accounts/homepage')
-        else:
-            raise HTTPRedirect(c.PATH)
-
 cherrypy.tree.mount(Root(), c.PATH, c.APPCONF)
 static_overrides(join(c.MODULE_ROOT, 'static'))
 
