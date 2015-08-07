2448eaa84c4150957a8ecd9d10408fcb3f5c897f
Dominic Cerquetti <dom@magfest.org>
2015-08-07 13:58:43 -0400

    group edit page allows assignment of ribbons to new badges minor UI fixes to make the buttons slightly less confusing than before ribbon selection doesn't show when group is a dealer

--- uber/site_sections/groups.py
+++ uber/site_sections/groups.py
@@ -37,7 +37,8 @@ class Root:
             message = check(group)
             if not message:
                 session.add(group)
-                message = session.assign_badges(group, params['badges'], params['badge_type'])
+                ribbon_to_use = c.NO_RIBBON if not 'ribbon' in params else params['ribbon']
+                message = session.assign_badges(group, params['badges'], params['badge_type'], ribbon_to_use)
                 if not message and new_dealer and not (first_name and last_name and email and group.badges):
                     message = 'When registering a new Dealer, you must enter the name and email address of the group leader and must allocate at least one badge'
                 if not message:
