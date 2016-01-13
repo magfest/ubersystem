3c56a9408fb38af593ac5019870bf79153197716
timrxd <timxdowd@gmail.com>
2016-01-13 15:49:30 -0500

    Less whitespace

--- uber/models.py
+++ uber/models.py
@@ -1806,7 +1806,8 @@ class Tracking(MagModel):
                 with Session() as session:
                     attendee = session.query(Attendee).filter(Attendee.id == params['id']).first()
                     if (attendee == None):
-                        raise HTTPRedirect('../registration/error_attendee_not_found')
+                        return
+                        #raise HTTPRedirect('../registration/error_attendee_not_found')
                     Tracking.track(c.PAGE_VIEWED, attendee)
             # Looking at a group's details
             elif "groups" in url:
