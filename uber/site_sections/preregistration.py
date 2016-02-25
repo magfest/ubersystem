971f39bab1f1a60a3c3fc27a4af3d6e01cc1f7f2
Victoria Earl <kitsuta@gmail.com>
2016-02-24 22:00:15 -0500

    Fixes 500 error from merge The merge left out an indent, which caused the server to crash due to a syntax error.

--- uber/site_sections/preregistration.py
+++ uber/site_sections/preregistration.py
@@ -401,7 +401,7 @@ class Root:
             if old.first_name == attendee.first_name and old.last_name == attendee.last_name:
                 message = 'You cannot transfer your badge to yourself.'
             elif not message and (not params['first_name'] and not params['last_name']):
-            message = check(attendee, prereg=True)
+                message = check(attendee, prereg=True)
             if not message and (not params['first_name'] and not params['last_name']):
                 message = 'First and Last names are required.'
 
