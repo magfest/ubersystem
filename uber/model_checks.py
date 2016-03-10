d1c0c085e5e6de0dd5c6c8de201c8a0fea579f54
Caleb Smith <cschmittiey@users.noreply.github.com>
2016-03-09 17:50:02 -0800

    better wording

--- uber/model_checks.py
+++ uber/model_checks.py
@@ -65,7 +65,7 @@ def group_paid(group):
     try:
         amount = int(float(group.amount_paid))
         if amount < 0:
-            return 'Amount Paid must be a number higher than 0'
+            return 'Amount Paid must be a number that is 0 or higher.'
     except:
         return "What you entered for Amount Paid ({}) isn't even a number".format(group.amount_paid)
 
