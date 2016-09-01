d54afcb464a30cd8d30f24a2134f718f1cfd354c
Daniel Evans <migetman9@gmail.com>
2016-08-31 20:51:19 -0400

    Adds check if attendee has no name on confirm page, meaning they should register. Who has no name?

--- uber/site_sections/preregistration.py
+++ uber/site_sections/preregistration.py
@@ -426,7 +426,8 @@ class Root:
 
     def confirm(self, session, message='', return_to='confirm', undoing_extra='', **params):
         attendee = session.attendee(params, restricted=True)
-
+        if attendee.full_name == ' ' or '':
+            raise HTTPRedirect('index?message={}', 'You have no name, please register.')
         placeholder = attendee.placeholder
         if 'email' in params and not message:
             attendee.placeholder = False
