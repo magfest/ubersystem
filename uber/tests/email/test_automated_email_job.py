ba3b68bfb0abda5b85311b9bb35a2c66f8803537
Dominic Cerquetti <dom@magfest.org>
2016-11-11 11:17:59 -0500

    add permutations of config options to config test

--- uber/tests/email/test_automated_email_job.py
+++ uber/tests/email/test_automated_email_job.py
@@ -1,17 +1,27 @@
 from uber.tests.email.email_fixtures import *
 
+@pytest.fixture
+def send_all_emails_mock():
+    with patch.object(SendAllAutomatedEmailsJob, '_send_all_emails', return_value=None) as mock:
+        yield mock
+
 
 @pytest.mark.usefixtures("email_subsystem_sane_setup")
 class TestSendAllAutomatedEmailsJob:
-    def test_enabled(self):
-        with patch.object(SendAllAutomatedEmailsJob, '_send_all_emails', return_value=None) as _send_all_emails:
-            SendAllAutomatedEmailsJob().run()
+    @pytest.mark.parametrize("c_at_the_con, c_send_emails, c_dev_box, expected_result", [
+        (True, True, True, 0),
+        (True, False, True, 0),
+        (True, True, False, 0),
+        (True, False, False, 0),
+        (False, True, True, 1),
+        (False, False, True, 1),
+        (False, True, False, 1),
+        (False, False, False, 0),
+    ])
+    def test_run_when_config_changed(self, monkeypatch, send_all_emails_mock, c_at_the_con, c_send_emails, c_dev_box, expected_result):
+        monkeypatch.setattr(c, 'AT_THE_CON', c_at_the_con)
+        monkeypatch.setattr(c, 'SEND_EMAILS', c_send_emails)
+        monkeypatch.setattr(c, 'DEV_BOX', c_dev_box)
 
-        assert _send_all_emails.call_count == 1
-
-    def test_disabled(self, monkeypatch):
-        monkeypatch.setattr(c, 'SEND_EMAILS', False)
-        with patch.object(SendAllAutomatedEmailsJob, '_send_all_emails', return_value=None) as _send_all_emails:
-            SendAllAutomatedEmailsJob().run()
-
-        assert _send_all_emails.call_count == 0
+        SendAllAutomatedEmailsJob().run()
+        assert send_all_emails_mock.call_count == expected_result
