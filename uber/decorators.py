714b34b28fcb7ce8a1c44ed74d29840bf4cdfe18
Daniel Evans <migetman9@gmail.com>
2016-11-23 10:17:47 -0800

    add to multiline_zip_file

--- uber/decorators.py
+++ uber/decorators.py
@@ -112,6 +112,7 @@ def ajax_gettable(func):
 
 
 def multifile_zipfile(func):
+    func.site_mappable = True
     @wraps(func)
     def zipfile_out(self, session):
         zipfile_writer = BytesIO()
