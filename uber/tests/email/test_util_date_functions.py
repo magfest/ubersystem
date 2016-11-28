6baf281e24f08cd72ed7fb86d5f10139545555f8
Dominic Cerquetti <binary1230@gmail.com>
2016-11-27 22:12:03 -0500

    refactor most date tests into paramaterized format

--- uber/tests/email/test_util_date_functions.py
+++ uber/tests/email/test_util_date_functions.py
@@ -34,25 +34,58 @@ def today_plus_10(set_datebase_now_to_sept_15th):
 
 
 @pytest.mark.usefixtures("set_datebase_now_to_sept_15th")
-class TestDaysBefore_DateFunctions:
-    @pytest.mark.parametrize("todays_date_offset, deadline_offset, days, until, expected_result", [
+class Test_DateFunctions:
+    @pytest.mark.parametrize("which_class, todays_date_offset, deadline_offset, days, until, expected_result", [
+
+        # ------- days_before tests ----------
 
         # deadline of 10 days from now
-        (0, +10, 1,   None, False),
-        (0, +10, 10,  None, False),
-        (0, +10, 11,  None, True),
-        (0, +10, 15,  None, True),
+        (days_before, 0, +10, 1,   None, False),
+        (days_before, 0, +10, 10,  None, False),
+        (days_before, 0, +10, 11,  None, True),
+        (days_before, 0, +10, 15,  None, True),
 
         # deadline of 10 days ago
-        (0, -10, 1, None, False),
-        (0, -10, 9, None, False),
-        (0, -10, 10, None, False),
-        (0, -10, 11, None, False),
+        (days_before, 0, -10, 1, None, False),
+        (days_before, 0, -10, 9, None, False),
+        (days_before, 0, -10, 10, None, False),
+        (days_before, 0, -10, 11, None, False),
 
         # deadline of right now
-        (0, 0, 1, None, False),
+        (days_before, 0, 0, 1, None, False),
+
+        # change the date of now() and then run this:
+        # days_before(days=5, deadline=now+7, until=2)()
+        (days_before,  0, +7, 5, 2, False),
+        (days_before, +1, +7, 5, 2, False),
+        (days_before, +2, +7, 5, 2, False),
+        (days_before, +3, +7, 5, 2, True),
+        (days_before, +4, +7, 5, 2, True),
+        (days_before, +5, +7, 5, 2, False),
+        (days_before, +6, +7, 5, 2, False),
+        (days_before, +7, +7, 5, 2, False),
+
+        # ------- days_after tests ----------
+
+        # deadline of 10 days from now
+        (days_after, 0, +10, 1, None, False),
+        (days_after, 0, +10, 10, None, False),
+        (days_after, 0, +10, 11, None, False),
+        (days_after, 0, +10, 15, None, False),
+
+        # deadline of 10 days ago
+        (days_after, 0, -10, 1,  None, True),
+        (days_after, 0, -10, 9,  None, True),
+        (days_after, 0, -10, 10, None, False),
+        (days_after, 0, -10, 11, None, False),
+        (days_after, 0, -10, 15, None, False),
+
+        # deadline of right now
+        (days_after, 0, 0, 1, None, False),
     ])
-    def test_until_before(self, monkeypatch, todays_date_offset, deadline_offset, days, until, expected_result):
+    def test_until_before(self, monkeypatch, which_class, todays_date_offset, deadline_offset, days, until, expected_result):
+
+        # run a bunch of setup code, then run the test.
 
         # change today's date as an offset from September 15th
         monkeypatch.setattr(DateBase, 'now', Mock(return_value=sept_15th + timedelta(days=todays_date_offset)))
@@ -60,10 +93,20 @@ class TestDaysBefore_DateFunctions:
         # change deadline as an offset from September 15th
         deadline = sept_15th + timedelta(days=deadline_offset)
 
-        assert days_before(days=days, deadline=deadline, until=until)() == expected_result
+        kwargs = {'days': days, 'deadline': deadline}
 
-    # -------------
+        if until:
+            assert which_class == days_before
+            kwargs['until'] = until
 
+        assert which_class in [days_before, days_after, after, before]
+
+        # setup code is done, run the actual test:
+        assert which_class(**kwargs)() == expected_result
+
+
+@pytest.mark.usefixtures("set_datebase_now_to_sept_15th")
+class TestDaysBefore_DateFunctions:
     def test_no_deadline_set(self):
         assert not days_before(1, None)()
 
@@ -81,22 +124,6 @@ class TestDaysBefore_DateFunctions:
 
 @pytest.mark.usefixtures("set_datebase_now_to_sept_15th")
 class TestDaysAfter_DateFunctions:
-    def test_after_tomorrow(self, today_plus_10):
-        assert not days_after(1, today_plus_10)()
-        assert not days_after(10, today_plus_10)()
-        assert not days_after(11, today_plus_10)()
-        assert not days_after(15, today_plus_10)()
-
-    def test_not_after_yesterday(self, today_minus_10):
-        assert days_after(1, today_minus_10)()
-        assert days_after(9, today_minus_10)()
-        assert not days_after(10, today_minus_10)()
-        assert not days_after(11, today_minus_10)()
-        assert not days_after(15, today_minus_10)()
-
-    def test_not_after_today(self):
-        assert not days_after(1, DateBase.now())()
-
     def test_no_deadline_set(self):
         assert not days_after(1, None)()
 
