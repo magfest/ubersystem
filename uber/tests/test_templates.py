5edb1fa55f249c1f4b82efca3db4e18ff0eacf0d
Rob Ruana <rob@robruana.com>
2017-03-22 17:05:52 -0700

    Updates #2379: Uses config setting 'template_dirs' to collect template paths

--- uber/tests/test_templates.py
+++ uber/tests/test_templates.py
@@ -3,15 +3,29 @@ import pytest
 from uber.common import *
 
 
-def collect_template_paths(file_path):
+def guess_template_dirs(file_path):
+    if not file_path:
+        return []
+
     current_path = os.path.abspath(os.path.expanduser(file_path))
     while current_path != '/':
-        templates_root = os.path.join(current_path, 'templates')
-        if os.path.exists(templates_root):
-            return glob(os.path.join(templates_root, '*.html')) + \
-                   glob(os.path.join(templates_root, '**', '*.html'))
+        template_dir = os.path.join(current_path, 'templates')
+        if os.path.exists(template_dir):
+            return [template_dir]
         current_path = os.path.normpath(os.path.join(current_path, '..'))
     return []
+
+
+def collect_template_paths(file_path):
+    template_dirs = c.TEMPLATE_DIRS
+    if not template_dirs:
+        template_dirs = guess_template_dirs(file_path)
+
+    template_paths = []
+    for template_dir in template_dirs:
+        template_paths.extend(glob(os.path.join(template_dir, '*.html')))
+        template_paths.extend(glob(os.path.join(template_dir, '**', '*.html')))
+    return template_paths
 
 
 @pytest.mark.parametrize("template_path", collect_template_paths(__file__))
