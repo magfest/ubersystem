b5a0948aa77164d954e6744c22dfa738f371608f
Rob Ruana <rob@robruana.com>
2017-04-03 17:48:51 -0700

    Adds new timedelta filter required by #185 in the magprime repo (#2425)

--- uber/tests/test_custom_tags.py
+++ uber/tests/test_custom_tags.py
@@ -2,7 +2,8 @@ import pytest
 
 from uber.common import *
 from uber.custom_tags import (jsonize, linebreaksbr, datetime_local_filter,
-    datetime_filter, full_datetime_local, hour_day_local, time_day_local, timestamp)
+    datetime_filter, full_datetime_local, hour_day_local, time_day_local,
+    timedelta_filter, timestamp)
 
 
 class TestDatetimeFilters(object):
@@ -23,6 +24,36 @@ class TestDatetimeFilters(object):
     ])
     def test_filters_allow_empty_arg(self, filter_function, test_input, expected):
         assert expected == filter_function(test_input)
+
+    @pytest.mark.parametrize('timedelta_args,timedelta_kwargs', [
+        ([], {}),
+        ([], dict(days=1, seconds=30, microseconds=100, milliseconds=100, minutes=20, hours=12, weeks=2)),
+        ([1, 30, 100, 100, 20, 12, 2], {}),
+    ])
+    def test_timedelta_filter(self, timedelta_args, timedelta_kwargs):
+        dt = datetime.utcnow()
+        td = timedelta(*timedelta_args, **timedelta_kwargs)
+        expected = dt + td
+        assert expected == timedelta_filter(dt, *timedelta_args, **timedelta_kwargs)
+
+    def test_timedelta_filter_with_empty_date(self):
+        assert timedelta_filter(dt=None, days=1, seconds=3600) is None
+        assert timedelta_filter(dt='', days=1, seconds=3600) is None
+        assert timedelta_filter(None, 1, 3600) is None
+        assert timedelta_filter('', 1, 3600) is None
+
+    def test_timedelta_filter_in_template(self):
+        dt = datetime.utcnow()
+        env = JinjaEnv.env()
+        template = env.from_string('{{ dt|timedelta(days=-5)|datetime("%A, %B %-e") }}')
+        expected = (dt + timedelta(days=-5)).strftime("%A, %B %-e")
+        assert expected == template.render(dt=dt)
+
+    def test_timedelta_filter_in_template_with_empty_date(self):
+        env = JinjaEnv.env()
+        template = env.from_string('{{ dt|timedelta(days=-5)|datetime("%A, %B %-e") }}')
+        expected = ''
+        assert expected == template.render(dt=None)
 
 
 class TestJsonize(object):
