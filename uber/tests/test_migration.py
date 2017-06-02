43a0d38ffdbf1067622bef27ec7bb20a44f65eb1
Rob Ruana <rob@robruana.com>
2017-06-02 11:35:49 -0400

    More fine-grained migration test skipping logic

--- uber/tests/test_migration.py
+++ uber/tests/test_migration.py
@@ -1,11 +1,12 @@
 import pytest
 import sqlite3
+from uber.models import Session
 from uber.tests import dump_alembic_schema, dump_reset_uber_db_schema
 
 
-@pytest.mark.skipif(
-    sqlite3.sqlite_version_info < (3, 8),
-    reason='requires recent version of sqlite')
 def test_alembic_migrations():
+    if Session.engine.dialect.name == 'sqlite' and \
+            sqlite3.sqlite_version_info < (3, 8):
+        pytest.skip('requires recent version of sqlite')
     assert dump_alembic_schema() == dump_reset_uber_db_schema(), (
         'Missing or invalid migrations, see alembic/README.md')
