2c6893d839fe34784113bd4a9a0f2d0143dc9af1
Rob Ruana <rob@robruana.com>
2017-06-01 20:35:56 -0400

    Skips migration tests for older versions of sqlite

--- uber/tests/test_migration.py
+++ uber/tests/test_migration.py
@@ -1,6 +1,11 @@
+import pytest
+import sqlite3
 from uber.tests import dump_alembic_schema, dump_reset_uber_db_schema
 
 
+@pytest.mark.skipif(
+    sqlite3.sqlite_version_info < (3, 8),
+    reason='requires recent version of sqlite')
 def test_alembic_migrations():
     assert dump_alembic_schema() == dump_reset_uber_db_schema(), (
         'Missing or invalid migrations, see alembic/README.md')
