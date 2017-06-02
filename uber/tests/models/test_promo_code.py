57c38774c016d6609b3675f0cf4c9a0bccba0cec
Rob Ruana <rob@robruana.com>
2017-06-02 17:41:43 -0400

    Adds some more tests for promo code stuff

--- uber/tests/models/test_promo_code.py
+++ uber/tests/models/test_promo_code.py
@@ -49,3 +49,95 @@ class TestPromoCodeUse:
             attendee._use_promo_code()
             assert getattr(attendee, attr) == value
             attendee.promo_code = None
+
+
+class TestPromoCodeSessionMixin:
+    disambiguated_promo_code_id = 'b32a41bd-828f-4169-88fe-f7f3ad4138dc'
+
+    @pytest.fixture
+    def disambiguated_promo_code(self):
+        with Session() as session:
+            session.add(PromoCode(
+                id=self.disambiguated_promo_code_id, code='012568'))
+
+        with Session() as session:
+            promo_code = session.query(PromoCode).filter(
+                PromoCode.id == self.disambiguated_promo_code_id).one()
+            yield promo_code
+            session.delete(promo_code)
+
+    def test_lookup_promo_code(self, disambiguated_promo_code):
+        promo_code_id = disambiguated_promo_code.id
+        with Session() as session:
+            for s1 in '0OQD':
+                for s2 in '1IL':
+                    for s3 in '2Z':
+                        for s4 in '5S':
+                            for s5 in '6G':
+                                for s6 in '8B':
+                                    letters = [s1, s2, s3, s4, s5, s6]
+
+                                    for s in ['', '-', ' ', '  ', '  --  -  ']:
+                                        code = s.join(letters)
+                                        p = session.lookup_promo_code(code)
+                                        assert p.id == promo_code_id
+
+                                        code = code.lower()
+                                        p = session.lookup_promo_code(code)
+                                        assert p.id == promo_code_id
+
+    def test_lookup_promo_code_nonexistent(self):
+        with Session() as session:
+            assert session.lookup_promo_code(None) is None
+            assert session.lookup_promo_code('') is None
+            assert session.lookup_promo_code('NONEXISTENT') is None
+
+    def test_attendee_promo_code_attr(self, disambiguated_promo_code):
+        """
+        Test that setting promo_code & promo_code_id have no affect on each
+        other. We're testing this because the Attendee.promo_code relationship
+        does not use the "save-update" cascade relationship, so these two
+        properties should not update each other.
+        """
+        attendee = Attendee()
+
+        attendee.promo_code = disambiguated_promo_code
+        assert attendee.promo_code is not None
+        assert attendee.promo_code_id is None
+
+        attendee.promo_code_id = disambiguated_promo_code.id
+        assert attendee.promo_code is not None
+        assert attendee.promo_code_id is not None
+
+        attendee.promo_code = None
+        assert attendee.promo_code is None
+        assert attendee.promo_code_id is not None
+
+        attendee.promo_code_id = disambiguated_promo_code.id
+        assert attendee.promo_code is None
+        assert attendee.promo_code_id is not None
+
+        attendee.promo_code_id = None
+        assert attendee.promo_code is None
+        assert attendee.promo_code_id is None
+
+    @pytest.mark.parametrize('code,message,promo_code_id,promo_code_code', [
+        (None, '', None, ''),
+        ('', '', None, ''),
+        ('012568', '', disambiguated_promo_code_id, '012568'),
+        ('olzsgb', '', disambiguated_promo_code_id, '012568'),
+        ('NONEXISTENT', 'The promo code you entered is invalid.', None, '')
+    ])
+    def test_add_promo_code_to_attendee(
+            self,
+            code,
+            message,
+            promo_code_id,
+            promo_code_code,
+            disambiguated_promo_code):
+
+        with Session() as session:
+            a = Attendee()
+            assert message == session.add_promo_code_to_attendee(a, code)
+            assert a.promo_code_id == promo_code_id
+            assert a.promo_code_code == promo_code_code
