2d6ae482607e9959eec91ec8cc6f912e92a9fd0e
kitsuta@gmail.com <kitsuta@gmail.com>
2017-06-22 10:12:02 -0400

    Don't redirect the prereg page if DEV_BOX is true I had to do this anyway to test other changes. Fixes https://github.com/magfest/ubersystem/issues/2580.

--- uber/decorators.py
+++ uber/decorators.py
@@ -43,7 +43,9 @@ def redirect_if_at_con_to_kiosk(func):
 def check_if_can_reg(func):
     @wraps(func)
     def with_check(*args, **kwargs):
-        if c.BADGES_SOLD >= c.MAX_BADGE_SALES:
+        if c.DEV_BOX:
+            pass  # Don't redirect to any of the pages below.
+        elif c.BADGES_SOLD >= c.MAX_BADGE_SALES:
             return render('static_views/prereg_soldout.html')
         elif c.BEFORE_PREREG_OPEN:
             return render('static_views/prereg_not_yet_open.html')
