92a7d4a67af0d8bb61a8e422895c536267fdf2af
kitsuta@gmail.com <kitsuta@gmail.com>
2017-07-28 14:59:24 -0400

    Add extra_donation column and config option This used to exist only in a couple event plugins, but now it's a common enough want that we're moving it into the core plugin and using a config option to show or hide the field itself.

--- uber/model_checks.py
+++ uber/model_checks.py
@@ -416,6 +416,16 @@ def out_of_badge_type(attendee):
                 return 'There are no more badges available for that type'
 
 
+@validation.Attendee
+def extra_donation_valid(attendee):
+    try:
+        extra_donation = int(float(attendee.extra_donation if attendee.extra_donation else 0))
+        if extra_donation < 0:
+            return 'Extra Donation must be a number that is 0 or higher.'
+    except:
+        return "What you entered for Extra Donation ({}) isn't even a number".format(attendee.extra_donation)
+
+
 @validation.MPointsForCash
 @validation.OldMPointExchange
 def money_amount(model):
