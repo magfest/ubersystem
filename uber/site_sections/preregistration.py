faa0d001d8a54b64bdfb0a346d2868c30dff240b
floraline <rturner22@gmail.com>
2017-09-07 18:01:07 -0400

    Delete badge when attendee abandons their group
    When an attendee abandons their badge, and they are part of a group,
    delete the badge instead of invalidating it. Do not allow an attendee
    to delete their badge if they are a group leader.
    
    Fixes #1669

--- uber/site_sections/preregistration.py
+++ uber/site_sections/preregistration.py
@@ -562,10 +562,25 @@ class Root:
     def not_found(self, id, message):
         return {'id': id, 'message': message}
 
-    def invalidate(self, session, id):
+    def abandon_badge(self, session, id):
         attendee = session.attendee(id)
-        attendee.badge_status = c.INVALID_STATUS
-        raise HTTPRedirect('invalid_badge?id={}&message={}', attendee.id, 'Sorry you can\'t make it! We hope to see you next year!')
+        success_message = "Sorry you can't make it! We hope to see you next year!"
+        failure_message = "You cannot abandon your badge because you are the leader of a group."
+
+        # if attendee is part of a group, we must delete attendee and remove from the group
+        if attendee.group:
+            if attendee.group.leader_id != attendee.id:
+                # remove this attendee from the group and delete the attendee
+                session.assign_badges(attendee.group, attendee.group.badges + 1, new_badge_type=attendee.badge_type, new_ribbon_type=attendee.ribbon, registered=attendee.registered, paid=attendee.paid)
+                session.delete_from_group(attendee, attendee.group)
+                raise HTTPRedirect('not_found?id={}&message={}', attendee.id, success_message)
+            else:
+                # attendee cannot invalidate their badge, as they are the leader of a group
+                raise HTTPRedirect('confirm?id={}&message={}', id, failure_message)
+        # otherwise, we will mark as invalid
+        else:
+            attendee.badge_status = c.INVALID_STATUS
+            raise HTTPRedirect('invalid_badge?id={}&message={}', attendee.id, success_message)
 
     def badge_updated(self, session, id, message=''):
         return {'id': id, 'message': message}
@@ -611,7 +626,8 @@ class Root:
             'attendee':      attendee,
             'message':       message,
             'affiliates':    session.affiliates(),
-            'badge_cost':    attendee.badge_cost if attendee.paid != c.PAID_BY_GROUP else 0
+            'badge_cost':    attendee.badge_cost if attendee.paid != c.PAID_BY_GROUP else 0,
+            'can_abandon':   not attendee.amount_paid and not attendee.paid == c.NEED_NOT_PAY and not (attendee.group and attendee.group.leader_id == attendee.id)
         }
 
     @id_required(Attendee)
