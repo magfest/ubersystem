4cb099381f88d9d2b122196fd4fba55729fc481b
Rob Ruana <rob@robruana.com>
2017-11-05 20:59:42 -0500

    Fixes staffer login

--- uber/site_sections/signups.py
+++ uber/site_sections/signups.py
@@ -125,9 +125,9 @@ class Root:
                 attendee = session.lookup_attendee(first_name.strip(), last_name.strip(), email, zip_code)
                 if not attendee.staffing:
                     message = safe_string('You are not signed up as a volunteer.  <a href="volunteer?id={}">Click Here</a> to sign up.'.format(attendee.id))
-                elif not attendee.assigned_depts_ints and not c.AT_THE_CON:
+                elif not attendee.dept_memberships and not c.AT_THE_CON:
                     message = 'You have not been assigned to any departments; an admin must assign you to a department before you can log in'
-            except:
+            except Exception as ex:
                 message = 'No attendee matches that name and email address and zip code'
 
             if not message:
