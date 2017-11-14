a78b2bb18d714b0ed2f94e12335fd59aa1197c03
binary1230 <dom@magfest.org>
2017-11-13 19:12:07 -0500

    fix error when exception handler context code deals with attachments - convert everything to String type first before doing anything with - this works fine even with objects that don't convert to string - will return something like ''<object Part at &ffffff>' - add some unit tests and mock data to armor this up some more

---
+++ uber/tests/test_exception_handler.py
@@ -0,0 +1,34 @@
+from uber import config
+from uber.tests import *
+from cherrypy._cpreqbody import Part
+
+
+class MockPart(Part):
+    # fake a cherrypy attachment, make it able to create without constructor
+    def __init__(self):
+        pass
+
+
+class TestExceptionHandler:
+    @pytest.fixture(autouse=True)
+    def setup_cherrypy_fake_env(self, monkeypatch):
+        monkeypatch.setattr(AdminAccount, 'admin_name', Mock(return_value='Bruce'))
+        monkeypatch.setattr(cherrypy.request, 'request_line', "/uber/location3/hello")
+        monkeypatch.setattr(cherrypy.request, 'params', {
+            'id': '32',
+            'action': 'reload',
+            'thing': 3,                 # use a non-string
+            'attachment': MockPart(),   # add fake attachment based on cherrypy's Part() class, make sure we handle OK
+        })
+        monkeypatch.setattr(cherrypy, 'session', {'session_id': '762876'})
+        headers = [
+            ('Content-Type', 'text/html'),
+            ('Server', 'Null CherryPy'),
+            ('Date', localized_now()),
+            ('Content-Length', '80'),
+        ]
+        monkeypatch.setattr(cherrypy.request, 'header_list', headers)
+
+    def test_exception_handler(self):
+        for expected in ['Request', 'text/html', 'reload', 'Bruce', 'session_id', 'Content-Length']:
+            assert expected in server.get_verbose_request_context()
