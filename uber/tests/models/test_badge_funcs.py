265621c0854ddf91f17d7a32c8c3c592c5766017
Rob Ruana <rob@robruana.com>
2017-11-18 21:41:27 -0500

    Updates staff badge creation logic to only allow STOPS and Registration dept heads to create staff badges after the deadline

--- uber/tests/models/test_badge_funcs.py
+++ uber/tests/models/test_badge_funcs.py
@@ -1,3 +1,4 @@
+from uber.config import c
 from uber.tests import *
 from uber.badge_funcs import needs_badge_num, reset_badge_if_unchanged
 
@@ -452,12 +453,17 @@ class TestBadgeValidations:
         session.regular_attendee.badge_num = None
         assert 'Custom badges have already been ordered so you cannot use this badge type' == check(session.regular_attendee)
 
-    def test_more_custom_badges_for_dept_head(self, admin_attendee, session, monkeypatch, after_printed_badge_deadline):
-        monkeypatch.setattr(Attendee, 'is_dept_head', lambda s: True)
+    @pytest.mark.parametrize('department,expected', [
+        (c.STOPS, None),
+        (c.REGDESK, None),
+        (c.CONSOLE, 'Custom badges have already been ordered so you cannot use this badge type'),
+    ])
+    def test_more_custom_badges_for_dept_head(self, admin_attendee, session, monkeypatch, after_printed_badge_deadline, department, expected):
+        monkeypatch.setattr(Attendee, 'is_dept_head_of', lambda s, d: d == department)
         session.regular_attendee.badge_type = session.regular_attendee.badge_type
         session.regular_attendee.badge_type = c.STAFF_BADGE
         session.regular_attendee.badge_num = None
-        assert not check(session.regular_attendee)
+        assert expected == check(session.regular_attendee)
 
     def test_out_of_badge_type(self, session, monkeypatch, before_printed_badge_deadline):
         monkeypatch.setitem(c.BADGE_RANGES, c.STAFF_BADGE, [1, 5])
