acbbeba08390bc5736766268c21ab09d72461bdf
Victoria Earl <kitsuta@gmail.com>
2018-08-28 13:30:38 -0400

    Let magprime add its own child badge
    Also changes the name to be a little more explicit

--- uber/models/attendee.py
+++ uber/models/attendee.py
@@ -589,9 +589,7 @@ class Attendee(MagModel, TakesPaymentMixin):
         registered = self.registered_local if self.registered else None
         base_badge_price = self.base_badge_price or c.get_attendee_price(registered)
 
-        attendee_badges = [c.ATTENDEE_BADGE]
-        if getattr(c, 'CHILD_BADGE', None):
-            attendee_badges.append(c.CHILD_BADGE)
+        age_discountable_badge_types = [c.ATTENDEE_BADGE]
 
         if self.paid == c.NEED_NOT_PAY:
             return 0
@@ -599,7 +597,7 @@ class Attendee(MagModel, TakesPaymentMixin):
             return self.overridden_price
         elif self.is_dealer:
             return c.DEALER_BADGE_PRICE
-        elif self.badge_type in attendee_badges and self.age_discount != 0:
+        elif self.badge_type in age_discountable_badge_types and self.age_discount != 0:
             return max(0, base_badge_price + self.age_discount)
         elif self.badge_type == c.ATTENDEE_BADGE and self.group and self.paid == c.PAID_BY_GROUP:
             return base_badge_price - c.GROUP_DISCOUNT
