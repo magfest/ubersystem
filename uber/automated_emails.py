b2713017f23c03c00dbfc71d125a81b28561af45
Joel Gonzales <LowPolyCount@users.noreply.github.com>
2018-12-19 16:48:23 -0500

    Email for new MIVS Checklist Items (#3398)
    * Create 2019_HandbookTrainingUpdate.txt
    
    * Update automated_emails.py
    
    * fixing whitespace

--- uber/automated_emails.py
+++ uber/automated_emails.py
@@ -723,6 +723,13 @@ if c.MIVS_ENABLED:
 
     MIVSEmailFixture(
         IndieGame,
+        'MIVS {EVENT_YEAR}: Indie Handbook and MIVS Training',
+        'mivs/accepted/2019_HandbookTrainingUpdate.txt',
+        lambda game: game.confirmed,
+        ident='2019_Handbook_Training_Update')
+
+    MIVSEmailFixture(
+        IndieGame,
         'Summary of judging feedback for your game',
         'mivs/reviews_summary.html',
         lambda game: game.status in c.FINAL_MIVS_GAME_STATUSES and game.reviews_to_email,
