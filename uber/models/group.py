9dc8f690b3bbfaec3b6bcec087f934cc870b28bc
Victoria Earl <kitsuta@gmail.com>
2019-05-27 11:31:54 -0400

    Use the max_dealers if it is set Fixes https://jira.magfest.net/browse/MAGDEV-491. We now use max_dealers as the actual maximum number of badges, unless it is not set, in which case we default to letting them have the number of tables they order plus 1. The dealer application page handles both cases.

--- uber/models/group.py
+++ uber/models/group.py
@@ -210,7 +210,7 @@ class Group(MagModel, TakesPaymentMixin):
 
     @property
     def dealer_max_badges(self):
-        return math.ceil(self.tables) + 1
+        return c.MAX_DEALERS or math.ceil(self.tables) + 1
 
     @property
     def dealer_badges_remaining(self):
