f8e06641aab6348642e95ecdadd99ceba8692cc8
Victoria Earl <kitsuta@gmail.com>
2019-07-19 13:27:46 -0400

    Retool how Staff and Event shirts interact to allow more flexibility The way we handled staff and event shirts was not flexible enough for MAGWest's purposes. I've replaced our `second_shirt` column with `num_event_shirts`, which allows events to add configuration for any number of shirts along with c.SHIRTS_PER_STAFFER. For example, an event can now have three shirts per staffer and let them replace either one or two (or none) of them, using just config.
    We do still assume that staffers (and not volunteers) are the only ones who get this selection, but this can be overridden using two properties (which you'll see shortly with MAGWest). The template for selecting shirt type/size is also still using Super MAGFest text, which we may want to move later or just override for other events that want to use this event-shirt-replacing selection.

---
+++ alembic/versions/8f8419ebcf27_replace_second_shirt_column_with_more_.py
@@ -0,0 +1,61 @@
+"""Replace second_shirt column with more flexible num_event_shirts column
+
+Revision ID: 8f8419ebcf27
+Revises: e0c620d341cb
+Create Date: 2019-07-19 16:31:05.311139
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = '8f8419ebcf27'
+down_revision = 'e0c620d341cb'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+try:
+    is_sqlite = op.get_context().dialect.name == 'sqlite'
+except Exception:
+    is_sqlite = False
+
+if is_sqlite:
+    op.get_context().connection.execute('PRAGMA foreign_keys=ON;')
+    utcnow_server_default = "(datetime('now', 'utc'))"
+else:
+    utcnow_server_default = "timezone('utc', current_timestamp)"
+
+def sqlite_column_reflect_listener(inspector, table, column_info):
+    """Adds parenthesis around SQLite datetime defaults for utcnow."""
+    if column_info['default'] == "datetime('now', 'utc')":
+        column_info['default'] = utcnow_server_default
+
+sqlite_reflect_kwargs = {
+    'listeners': [('column_reflect', sqlite_column_reflect_listener)]
+}
+
+# ===========================================================================
+# HOWTO: Handle alter statements in SQLite
+#
+# def upgrade():
+#     if is_sqlite:
+#         with op.batch_alter_table('table_name', reflect_kwargs=sqlite_reflect_kwargs) as batch_op:
+#             batch_op.alter_column('column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#     else:
+#         op.alter_column('table_name', 'column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#
+# ===========================================================================
+
+
+def upgrade():
+    op.add_column('attendee', sa.Column('num_event_shirts', sa.Integer(), server_default='0', nullable=False))
+    op.drop_column('attendee', 'second_shirt')
+
+
+def downgrade():
+    op.add_column('attendee', sa.Column('second_shirt', sa.INTEGER(), server_default=sa.text('194196342'), autoincrement=False, nullable=False))
+    op.drop_column('attendee', 'num_event_shirts')
