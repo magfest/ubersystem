d15c5ccb8e42123591995bf736733b3bfa50a46a
Robert Earl <r.a.earl@gmail.com>
2019-09-08 18:01:24 -0400

    Fix Extra Donation bug
    updated check to mirror the one coded for amount_extra

--- uber/model_checks.py
+++ uber/model_checks.py
@@ -523,7 +523,7 @@ def invalid_badge_name(attendee):
 @validation.Attendee
 def extra_donation_valid(attendee):
     try:
-        extra_donation = int(float(attendee.extra_donation if attendee.extra_donation else 0))
+        extra_donation = int(float(attendee.extra_donation or 0))
         if extra_donation < 0:
             return 'Extra Donation must be a number that is 0 or higher.'
     except Exception:
