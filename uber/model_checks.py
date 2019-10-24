f3150b0971c191179fe08e70d8c079b0e3e35af2
Victoria Earl <kitsuta@gmail.com>
2019-10-24 01:22:10 -0400

    Fix minor style issues

--- uber/model_checks.py
+++ uber/model_checks.py
@@ -483,8 +483,7 @@ def out_of_badge_type(attendee):
             
 @validation.Attendee
 def not_in_range(attendee):
-    lower_bound = c.BADGE_RANGES[attendee.badge_type][0]
-    upper_bound = c.BADGE_RANGES[attendee.badge_type][1]
+    lower_bound, upper_bound = c.BADGE_RANGES[attendee.badge_type]
     if attendee.badge_num and not (lower_bound <= attendee.badge_num <= upper_bound):
         return 'Badge number {} is out of range for badge type {} ({} - {})'.format(attendee.badge_num, 
                                                                                     attendee.badge_type, 
