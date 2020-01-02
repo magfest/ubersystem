504d7ab81822313af8ba86bef4b30d79c6a78319
Victoria Earl <kitsuta@gmail.com>
2020-01-02 01:29:49 -0500

    Allow adding numbers to attendees during at-con mode (#3704)
    Fixes https://github.com/magfest/ubersystem/issues/3703. Normally we zero out a badge number if an attendee doesn't need one. This doesn't make sense for at-the-con, when we may need to assign badge numbers ahead of time. So if we're at the con, we don't zero out the number. We also don't auto-assign badge numbers -- we throw an error if you try to check in someone without one. Also also, we no longer make "save and check-in" the default action when you press enter on the form.

--- uber/models/attendee.py
+++ uber/models/attendee.py
@@ -535,7 +535,7 @@ class Attendee(MagModel, TakesPaymentMixin):
         old_type = self.orig_value_of('badge_type')
         old_num = self.orig_value_of('badge_num')
 
-        if not needs_badge_num(self):
+        if not needs_badge_num(self) and not c.AT_THE_CON:
             self.badge_num = None
 
         if old_type != self.badge_type or old_num != self.badge_num:
