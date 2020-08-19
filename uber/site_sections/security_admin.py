1a4adc7292f46dc117aba27ac90c03e12c22df4c
Victoria Earl <kitsuta@gmail.com>
2020-08-19 15:53:40 -0400

    Move watchlist entries to their own form Fixes https://jira.magfest.net/browse/MAGDEV-915. We don't use a modal here because watchlists, much like groups, can be associated with attendees so we need to reserve the modal for them.

--- uber/site_sections/security_admin.py
+++ uber/site_sections/security_admin.py
@@ -4,6 +4,7 @@ from uber.config import c
 from uber.decorators import ajax, all_renderable, log_pageview
 from uber.errors import HTTPRedirect
 from uber.models import Attendee, WatchList
+from uber.utils import check
 
 
 @all_renderable()
@@ -15,40 +16,44 @@ class Root:
             if entry.active:
                 entry.attendee_guesses = session.guess_watchentry_attendees(entry)
 
-        if 'first_names' in params:
-            watch_entry = session.watch_list(params, bools=WatchList.all_bools)
+        return {
+            'watchlist_entries': watchlist_entries,
+            'message': message
+        }
 
-            if not watch_entry.first_names and not watch_entry.last_name:
-                message = 'A first or last name is required.'
-            if not watch_entry.email and not watch_entry.birthdate:
-                message = 'Email or date of birth is required.'
-            elif not watch_entry.reason or not watch_entry.action:
-                message = 'Reason and action are required.'
+    def watchlist_form(self, session, message='', **params):
+        if 'attendee_id' in params:
+            attendee = session.attendee(params.get('attendee_id'), allow_invalid=True)
+            entry = WatchList(first_names=attendee.first_name,
+                              last_name=attendee.last_name,
+                              email=attendee.email,
+                              birthdate=attendee.birthdate)
+        else:
+            entry = session.watch_list(params, bools=['active'])
+        entry.attendee_guesses = session.guess_watchentry_attendees(entry)
+
+        if cherrypy.request.method == 'POST':
+            message = check(entry)
+            newly_banned_attendees = []
 
             if not message:
-                session.add(watch_entry)
-                for attendee in session.guess_watchentry_attendees(watch_entry):
-                    if attendee.badge_status == c.NEW_STATUS:
-                        attendee.badge_status = c.WATCHED_STATUS
-                        session.add(attendee)
+                session.add(entry)
+
+                if entry.active:
+                    for attendee in session.guess_watchentry_attendees(entry):
+                        if attendee.badge_status == c.NEW_STATUS:
+                            attendee.badge_status = c.WATCHED_STATUS
+                            newly_banned_attendees.append(attendee.full_name)
+                            session.add(attendee)
+
                 if 'id' not in params:
-                    message = 'New watch list item added.'
+                    message = 'New watchlist entry added.'
                 else:
-                    message = 'Watch list item updated.'
-
-                session.commit()
-        elif 'attendee_id' in params:
-            attendee = session.attendee(params.get('attendee_id'), allow_invalid=True)
-            watch_entry = WatchList(first_names=attendee.first_name,
-                                    last_name=attendee.last_name,
-                                    email=attendee.email,
-                                    birthdate=attendee.birthdate)
-        else:
-            watch_entry = WatchList()
-
+                    message = 'Watchlist entry updated.'
+                raise HTTPRedirect('index?message={}', message)
+                
         return {
-            'new_watch': watch_entry,
-            'watchlist_entries': watchlist_entries,
+            'entry': entry,
             'message': message
         }
 
