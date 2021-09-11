49fc6c011576b8ae77475672119fb4048d212d3d
Victoria Earl <kitsuta@gmail.com>
2021-09-11 00:20:48 -0400

    Add login option to prereg workflow Attendees can now log into their account even after starting the prereg process. Also improves the landing page to reflect logged-in status.

--- uber/site_sections/landing.py
+++ uber/site_sections/landing.py
@@ -3,11 +3,12 @@ from uber.decorators import all_renderable
 
 @all_renderable(public=True)
 class Root:
-    def index(self, **params):
+    def index(self, session, **params):
         return {
             'message': params.get('message', ''),
             'email':   params.get('email', ''),
             'original_location': params.get('original_location'),
+            'logged_in_account': session.current_attendee_account(),
         }
 
     def invalid(self, **params):
