4fdeceadc67f376dd2343a07ca7f0149ebf2de38
Mark Murnane <mark@hackafe.net>
2022-09-04 18:55:07 -0400

    Allow pulling covid_ready status via API
    It would be nice for the covid verification system to be able to pull the current covid_ready status out of uber.

--- uber/api.py
+++ uber/api.py
@@ -324,6 +324,7 @@ class AttendeeLookup:
         'is_dept_head': True,
         'ribbon_labels': True,
         'public_id': True,
+        'covid_ready': True,
     }
 
     fields_full = dict(fields, **{
