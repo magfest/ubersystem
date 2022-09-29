e8a2d5d6b3a5c8ba40e1cd39b37995c35f72f42f
Victoria Earl <kitsuta@gmail.com>
2022-09-29 17:18:35 +0200

    Update attendee before charging for upgrades

--- uber/site_sections/preregistration.py
+++ uber/site_sections/preregistration.py
@@ -1441,6 +1441,7 @@ class Root:
         if message:
             session.rollback()
             return {'error': message}
+        session.add(attendee)
         session.commit()
 
         return {'success': True}
