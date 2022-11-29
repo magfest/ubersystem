c7a914a666cf3459ab3f5c93c5604155a15b584e
Victoria Earl <kitsuta@gmail.com>
2022-11-29 16:13:39 -0500

    Improve min badges addable checks There were some cases where we would tell groups they could add badges, but they couldn't actually add them because we weren't in PRE_CON mode. This fixes that.

--- uber/models/group.py
+++ uber/models/group.py
@@ -381,6 +381,8 @@ class Group(MagModel, TakesPaymentMixin):
 
     @property
     def min_badges_addable(self):
+        if not c.PRE_CON:
+            return 0
         if self.is_dealer and not self.dealer_badges_remaining or self.amount_unpaid:
             return 0
         if self.can_add:
