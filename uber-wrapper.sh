0b830f34cc5d500e02099b9fd5d9655bf5e1cdd7
Victoria Earl <kitsuta@gmail.com>
2023-04-20 00:15:08 -0400

    Improve local development experience - Adds a Python REPL container, which has the profile 'dev' to avoid being run in production - Uses YAML anchors where possible to reuse docker compose config - Separates out building config from the entrypoint command to a script that can be run at-will to update local config to match config in git - Adds instructions to docker-compose.override.yml.template

--- uber-wrapper.sh
+++ uber-wrapper.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 set -e
 
-# This will replace any variable references in these files
-# If you want to add any additional settings here just add
-# the variables to the environment when running this.
-envsubst < "uber-development.ini.template" > /app/plugins/uber/development.ini
-envsubst < "sideboard-development.ini.template" > /app/development.ini
-
-if [ -n "${UBERSYSTEM_GIT_CONFIG}" ]; then
-    echo "Loading UBERSYSTEM_CONFIG from git repo ${UBERSYSTEM_GIT_CONFIG}"
-    /app/env/bin/python /app/plugins/uber/make_config.py --repo "${UBERSYSTEM_GIT_CONFIG}" --paths ${UBERSYSTEM_GIT_CONFIG_PATHS}
-fi
-
-if [ -n "${UBERSYSTEM_CONFIG}" ]; then
-    echo "Parsing config from environment"
-    /app/env/bin/python /app/plugins/uber/make_config.py
-fi
-
 if [ "$1" = 'uber' ]; then
     echo "If this is the first time starting this server go to the following URL to create an account:"
     echo "http://${HOSTNAME}:${PORT}${DEFAULT_URL}/accounts/insert_test_admin"
