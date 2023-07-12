4802d2690365528a77aa9bb9b522e1a7eec11e06
Mark Murnane <mark@hackafe.net>
2023-07-12 15:46:43 -0700

    Enable celery debug logging

--- uber-wrapper.sh
+++ uber-wrapper.sh
@@ -36,9 +36,9 @@ if [ "$1" = 'uber' ]; then
     /app/env/bin/python3 /app/sideboard/sep.py alembic upgrade heads
     /app/env/bin/python3 /app/sideboard/run_server.py
 elif [ "$1" = 'celery-beat' ]; then
-    /app/env/bin/celery -A uber.tasks beat --pidfile=
+    /app/env/bin/celery -A uber.tasks beat --loglevel=DEBUG --pidfile=
 elif [ "$1" = 'celery-worker' ]; then
-    /app/env/bin/celery -A uber.tasks worker
-fi
-
+    /app/env/bin/celery -A uber.tasks worker --loglevel=DEBUG
+else
 exec "$@"
+fi
