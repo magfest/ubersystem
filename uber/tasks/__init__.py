4013702f40e5f3a5ccddaa288eda804d609988cb
Mark Murnane <mark@hackafe.net>
2023-08-02 21:35:18 -0400

    Replacing SRV with config options

--- uber/tasks/__init__.py
+++ uber/tasks/__init__.py
@@ -12,7 +12,13 @@ celery = Celery('tasks')
 celery.conf.beat_schedule = {}
 celery.conf.beat_startup_tasks = []
 celery.conf.update(config_dict['celery'])
-celery.conf.update(broker_url=config_dict['secret']['broker_url'])
+
+broker_url = config_dict['secret']['broker_url']
+
+celery.conf.update(broker_url=broker_url)
+celery.conf.update(result_backend=broker_url.replace("amqps://", "rpc://"))
+celery.conf.update(task_ignore_result=True)
+
 celery.conf.update(result_backend=config_dict['secret']['broker_url'].replace("amqp://", "rpc://"))
 celery.conf.update(task_ignore_result=True)
 
