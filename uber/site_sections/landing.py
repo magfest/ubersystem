73a0287c872b5abc4bf75659c44f4282e55356e7
Victoria Earl <kitsuta@gmail.com>
2023-08-22 19:31:29 -0400

    Add available badges to landing page This is kind of ugly, but what's important right now is that we have this info available without logging in.

--- uber/site_sections/landing.py
+++ uber/site_sections/landing.py
@@ -2,6 +2,8 @@ import cherrypy
 
 from uber.decorators import all_renderable, requires_account
 from uber.errors import HTTPRedirect
+from uber.forms import attendee as attendee_forms, load_forms
+from uber.models import Attendee
 
 
 @all_renderable(public=True)
@@ -9,12 +11,17 @@ class Root:
     def index(self, session, **params):
         if 'exit_kiosk' in params:
             cherrypy.session['kiosk_mode'] = False
+
+        forms = load_forms({}, Attendee(), attendee_forms, ['BadgeExtras'])
+
         return {
             'message': params.get('message', ''),
             'email':   params.get('email', ''),
             'original_location': params.get('original_location'),
             'logged_in_account': session.current_attendee_account(),
             'kiosk_mode': cherrypy.session.get('kiosk_mode'),
+            'badge_extras': forms['badge_extras'],
+            'attendee': Attendee(),
         }
 
     def invalid(self, **params):
