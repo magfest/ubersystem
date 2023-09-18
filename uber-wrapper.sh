92d7a80544338d662bf3ea01afa22f9deebbf540
Mark Murnane <mark@hackafe.net>
2023-09-18 17:54:23 -0400

    Update uber-wrapper.sh

--- uber-wrapper.sh
+++ uber-wrapper.sh
@@ -24,7 +24,8 @@ if [ "$1" = 'uber' ]; then
     echo "If this is the first time starting this server go to the following URL to create an account:"
     echo "http://${HOSTNAME}:${PORT}${DEFAULT_URL}/accounts/insert_test_admin"
     echo "From there the default login is magfest@example.com / magfest"
-    /app/env/bin/python3 /app/sideboard/sep.py alembic upgrade heads
+# Disabling migrations due to brokeness we don't care to fix on the super2020 branch
+#    /app/env/bin/python3 /app/sideboard/sep.py alembic upgrade heads
     /app/env/bin/python3 /app/sideboard/run_server.py
 elif [ "$1" = 'celery-beat' ]; then
     /app/env/bin/celery -A uber.tasks beat --pidfile=
