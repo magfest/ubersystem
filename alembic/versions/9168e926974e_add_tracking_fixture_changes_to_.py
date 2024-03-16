bff6fa08ed14b7f7792733000a65f79c46608ff8
Victoria Earl <kitsuta@gmail.com>
2024-03-15 22:19:39 -0400

    Allow updating email dates through code The previous change made it so that code updates to email dates NEVER worked. We now track which dates have been changed to avoid that. We also show admins a list of changes on each email and allow them to revert those changes back to the default.

---
+++ alembic/versions/9168e926974e_add_tracking_fixture_changes_to_.py
@@ -0,0 +1,59 @@
+"""Add tracking fixture changes to automated emails
+
+Revision ID: 9168e926974e
+Revises: e4a1d20d687b
+Create Date: 2024-03-15 23:27:27.119664
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = '9168e926974e'
+down_revision = 'e4a1d20d687b'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+try:
+    is_sqlite = op.get_context().dialect.name == 'sqlite'
+except Exception:
+    is_sqlite = False
+
+if is_sqlite:
+    op.get_context().connection.execute('PRAGMA foreign_keys=ON;')
+    utcnow_server_default = "(datetime('now', 'utc'))"
+else:
+    utcnow_server_default = "timezone('utc', current_timestamp)"
+
+def sqlite_column_reflect_listener(inspector, table, column_info):
+    """Adds parenthesis around SQLite datetime defaults for utcnow."""
+    if column_info['default'] == "datetime('now', 'utc')":
+        column_info['default'] = utcnow_server_default
+
+sqlite_reflect_kwargs = {
+    'listeners': [('column_reflect', sqlite_column_reflect_listener)]
+}
+
+# ===========================================================================
+# HOWTO: Handle alter statements in SQLite
+#
+# def upgrade():
+#     if is_sqlite:
+#         with op.batch_alter_table('table_name', reflect_kwargs=sqlite_reflect_kwargs) as batch_op:
+#             batch_op.alter_column('column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#     else:
+#         op.alter_column('table_name', 'column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#
+# ===========================================================================
+
+
+def upgrade():
+    op.add_column('automated_email', sa.Column('revert_changes', postgresql.JSONB(astext_type=sa.Text()), server_default='{}', nullable=False))
+
+
+def downgrade():
+    op.drop_column('automated_email', 'revert_changes')
