61c94323a0b797a81ee185cbbeffb1c5d6d86797
Mark Murnane <mark@hackafe.net>
2024-04-14 18:38:00 -0400

    Fixing args

--- uber/site_sections/api.py
+++ uber/site_sections/api.py
@@ -49,8 +49,8 @@ class Root:
                 if not method_name.startswith('_'):
                     method = unwrap(method)
                     doc = method.__doc__ or ''
-                    args = signature(method).parameters
-                    if 'self' in dict(args):
+                    args = dict(signature(method).parameters)
+                    if 'self' in args:
                         args.remove('self')
                     access = getattr(method, 'required_access', set())
                     required_access = sorted([opt[4:].title() for opt in access])
