d75c7b2875311ba5596d71542a045f8000f2ea59
Mark Murnane <mark@hackafe.net>
2024-04-22 00:46:31 -0400

    Fixing healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # This provides simple health checks for use in docker/k8s/etc.
 # Pass the task run by this container and it will exit non-zero if something is wrong
@@ -8,21 +8,24 @@ set -e
 RESULT_PROTOCOL=$(echo "${BROKER_PROTOCOL}" | sed 's/amqps/rpc/g;s/amqp/rpc/g')
 cat <<EOF > celeryconf.py
 # celery config used for celery cli-based health checks (Not loaded by ubersystem directly)
-broker_url = "${BROKER_PROTOCOL}://${BROKER_USER}:${BROKER_PASS}@${BROKER_HOST}:${BROKER_PORT}/${BROKER_VHOST}"
-result_backend = "${RESULT_PROTOCOL}://${BROKER_USER}:${BROKER_PASS}@${BROKER_HOST}:${BROKER_PORT}/${BROKER_VHOST}"
-result_backend_transport_options = {'global_prefix': "${BROKER_PREFIX}"}
+broker_url = "${uber_secret_broker_url}"
+result_backend = "${uber_secret_broker_url}"
+result_backend_transport_options = {'global_prefix': "${uber_secret_broker_prefix}"}
 EOF
 
 # If no arguments are passed grab the current CMD from the entrypoint
 CMD="${1:-$(cat /proc/1/cmdline | strings -1 | tail -1)}"
 
 if [ "$CMD" = 'uber' ]; then
-    curl --fail http://$HOST:$PORT/uber/devtools/health
+    curl --fail http://$HOST:$PORT/devtools/health
 elif [ "$CMD" = 'celery-beat' ]; then
     # Beat seems to do a good job of dying when things go wrong.
     # If you know a good way to test its health please put it here.
     exit 0
 elif [ "$CMD" = 'celery-worker' ]; then
     cd /app
-    /app/env/bin/celery --config celeryconf inspect ping -d celery@$(cat /etc/hostname)
+    /usr/local/bin/celery --config celeryconf inspect ping -d celery@$(cat /etc/hostname)
+else
+    echo "Unknown service"
+    exit 1
 fi
