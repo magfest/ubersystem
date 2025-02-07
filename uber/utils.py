a82117d40e71ff788260e45aef8bdef857da3638
hdaws <hayley.dawson@proton.me>
2025-02-06 23:02:45 -0500

    seems to work well in both collect birthday and not mode

--- uber/utils.py
+++ uber/utils.py
@@ -335,7 +335,7 @@ def get_age_conf_from_birthday(birthdate, today=None):
     age = get_age_from_birthday(birthdate, today)
 
     for val, age_group in c.AGE_GROUP_CONFIGS.items():
-        if val != c.AGE_UNKNOWN and age_group['min_age'] <= age and age <= age_group['max_age']:
+        if val != c.AGE_UNKNOWN and age_group['min_age'] <= age <= age_group['max_age']:
             return age_group
 
     return c.AGE_GROUP_CONFIGS[c.AGE_UNKNOWN]
