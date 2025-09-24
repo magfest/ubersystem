628aff6b64b121b5e306b22db845a60aa6db038e
Victoria Earl <kitsuta@gmail.com>
2025-09-24 10:53:33 +0200

    Give new emails unique idents Hahaha whoops

--- uber/automated_emails.py
+++ uber/automated_emails.py
@@ -1086,28 +1086,28 @@ if c.INDIE_ARCADE_START:
         'Your game has been accepted into the MAGFest Indie Arcade',
         'indie_arcade/game_accepted.txt',
         lambda game: game.status == c.ACCEPTED and not game.waitlisted and game.showcase_type == c.MIVS,
-        ident='mivs_game_accepted')
+        ident='ia_game_accepted')
 
     IAEmailFixture(
         IndieGame,
         'Your game has been accepted into the MAGFest Indie Arcade from our waitlist',
         'indie_arcade/game_accepted_from_waitlist.txt',
         lambda game: game.status == c.ACCEPTED and game.waitlisted and game.showcase_type == c.MIVS,
-        ident='mivs_game_accepted_from_waitlist')
+        ident='ia_game_accepted_from_waitlist')
 
     IAEmailFixture(
         IndieGame,
         'Your game application has been declined from the {c.EVENT_YEAR} Indie Arcade',
         'indie_arcade/game_declined.txt',
         lambda game: game.status == c.DECLINED and game.showcase_type == c.MIVS,
-        ident='mivs_game_declined')
+        ident='ia_game_declined')
 
     IAEmailFixture(
         IndieGame,
         'Your Indie Arcade application has been waitlisted',
         'indie_arcade/game_waitlisted.txt',
         lambda game: game.status == c.WAITLISTED and game.showcase_type == c.MIVS,
-        ident='mivs_game_waitlisted')
+        ident='ia_game_waitlisted')
 
 
 class MIVSEmailFixture(AutomatedEmailFixture):
