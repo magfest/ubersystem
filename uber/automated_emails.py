630584583cf8bec21d453bd2c447302cebb9e629
Victoria Earl <kitsuta@gmail.com>
2025-09-25 21:58:48 +0200

    Send guest placeholder emails to arena guests

--- uber/automated_emails.py
+++ uber/automated_emails.py
@@ -549,7 +549,7 @@ def panelist_placeholder(a): return a.placeholder and c.PANELIST_RIBBON in a.rib
 def guest_placeholder(a): return a.placeholder and a.badge_type == c.GUEST_BADGE and (
             not a.group
             or a.group.guest
-            and a.group.guest.group_type == c.GUEST)
+            and a.group.guest.group_type in [c.ARENA, c.GUEST])
 
 
 def band_placeholder(a): return a.placeholder and a.badge_type == c.GUEST_BADGE and (
