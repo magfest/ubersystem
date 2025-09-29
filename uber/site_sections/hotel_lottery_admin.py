8d1d6977922e98cdf0a9d84d8697a97244789db6
Sean VanderHeyden <sean.vanderheyden@gmail.com>
2025-09-29 10:59:14 -0400

    fixing typo

--- uber/site_sections/hotel_lottery_admin.py
+++ uber/site_sections/hotel_lottery_admin.py
@@ -122,7 +122,7 @@ def solve_lottery(applications, hotel_rooms, lottery_type=c.ROOM_ENTRY):
             entries[app.id] = entry
                     
    for app in applications:
-        if app.entry_type and app.parent_application in entries:
+        if app.entry_type == lottery_type and app.parent_application in entries:
             entries[app.parent_application]["members"].append(app)
 
     for app_id, entry in entries.items():
