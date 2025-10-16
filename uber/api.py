5e9a50822a48fb2118734b8249bfef362de67499
Victoria Earl <kitsuta@gmail.com>
2025-10-16 12:46:43 +0200

    Keep assigned depts for deferred staff Everyone who deferred last year was imported with no assigned departments this year because they didn't work any shifts in their departments. This carves out an exception for them so we won't have the same issue next year.

--- uber/api.py
+++ uber/api.py
@@ -232,7 +232,10 @@ def _prepare_attendees_export(attendees, include_account_ids=False, include_apps
             poc_depts = {}
             roles_depts = defaultdict(list)
 
-            active_dept_memberships = [m for m in a.dept_memberships if m.has_inherent_role or 
+            if a.badge_status == c.DEFERRED_STATUS:
+                active_dept_memberships = a.dept_memberships
+            else:
+                active_dept_memberships = [m for m in a.dept_memberships if m.has_inherent_role or 
                                            m.department in a.depts_where_working or m.department.is_shiftless]
 
             for membership in active_dept_memberships:
