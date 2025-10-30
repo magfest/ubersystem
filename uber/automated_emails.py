b40c3defe6afad3ad0730fae7c673d4e96f5b6ec
Victoria Earl <kitsuta@gmail.com>
2025-10-30 15:27:25 +0100

    Fix Indies checklist automated emails

--- uber/automated_emails.py
+++ uber/automated_emails.py
@@ -1045,7 +1045,7 @@ class RetroGuestEmailFixture(AutomatedEmailFixture):
             GuestGroup,
             subject,
             template,
-            lambda mg: mg.group_type == c.MIVS and mg.group.studio and mg.matches_showcases[c.INDIE_RETRO] and filter(mg),
+            lambda mg: mg.group_type == c.MIVS and mg.group.studio and mg.matches_showcases([c.INDIE_RETRO]) and filter(mg),
             ident,
             sender=c.INDIE_RETRO_EMAIL,
             **kwargs)
@@ -1088,7 +1088,7 @@ class IAGuestEmailFixture(AutomatedEmailFixture):
             GuestGroup,
             subject,
             template,
-            lambda mg: mg.group_type == c.MIVS and mg.group.studio and mg.matches_showcases[c.INDIE_ARCADE] and filter(mg),
+            lambda mg: mg.group_type == c.MIVS and mg.group.studio and mg.matches_showcases([c.INDIE_ARCADE]) and filter(mg),
             ident,
             sender=c.INDIE_ARCADE_EMAIL,
             **kwargs)
@@ -1158,7 +1158,7 @@ class MIVSGuestEmailFixture(AutomatedEmailFixture):
             GuestGroup,
             subject,
             template,
-            lambda mg: mg.group_type == c.MIVS and mg.group.studio and mg.matches_showcases[c.MIVS] and filter(mg),
+            lambda mg: mg.group_type == c.MIVS and mg.group.studio and mg.matches_showcases([c.MIVS]) and filter(mg),
             ident,
             sender=c.MIVS_EMAIL,
             **kwargs)
