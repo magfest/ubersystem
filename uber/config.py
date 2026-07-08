b8d630858c60002688b115d29a95d53f52949762
Victoria Earl <kitsuta@gmail.com>
2026-07-07 21:52:08 -0400

    Fix MIVS-related email permissions (again)

--- uber/config.py
+++ uber/config.py
@@ -1328,11 +1328,13 @@ class Config(_Overridable):
         email_dict = {
             c.MARKETPLACE_EMAIL: [c.MARKETPLACE_NOTIFICATIONS_EMAIL],
             c.ART_SHOW_EMAIL: [c.ART_SHOW_NOTIFICATIONS_EMAIL, c.ART_SHOW_BCC_EMAIL],
-            c.MIVS_EMAIL: [c.INDIE_SHOWCASE_EMAIL, c.INDIE_ARCADE_EMAIL, c.INDIE_RETRO_EMAIL],
-            c.INDIE_ARCADE_EMAIL: [c.INDIE_SHOWCASE_EMAIL, c.INDIE_RETRO_EMAIL, c.MIVS_EMAIL],
-            c.INDIE_RETRO_EMAIL: [c.INDIE_SHOWCASE_EMAIL, c.INDIE_ARCADE_EMAIL, c.MIVS_EMAIL],
         }
         email_dict.pop('', '')
+
+        indie_emails = [c.INDIE_SHOWCASE_EMAIL, c.INDIE_ARCADE_EMAIL, c.INDIE_RETRO_EMAIL, c.MIVS_EMAIL]
+
+        for email in indie_emails:
+            email_dict[email] = [e for e in indie_emails if e != email]
 
         # Run email_only on all the keys and values of email_dict and then return it
         return dict(map(lambda x: (email_only(x), list(map(email_only, email_dict[x]))), email_dict))
